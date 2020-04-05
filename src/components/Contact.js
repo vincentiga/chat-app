403778b3903720bbc825b5b07623b018f2762ddd
Vincent <igabillevincent@outlook.fr>
2020-04-05 17:56:25 +0200

    waiting for corection

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './Contact.css'
 
-const avatarPic = ('https://randomuser.me/api/portraits/women/35.jpg');
-const psuedo = 'Katherine Parker'
-const online = true
 
-const Contact = () => {
+
+const Contact = (props) => {
     return (
         <div className='Contact'>
-            <img src={avatarPic} className='avatar' alt='avatar_picture'></img>
-            <h4 className='name' >{psuedo}
+            
+            <img src={props.avatarPic} className='avatar' alt={props.character}></img>
+            <h4 className='name'>{props.characterFirstName + ' ' + props.characterLastName}
                 <div className='status'>
-                    <div className='status-online'></div> 
-                    <p className='status-text'> {online ? 'online' : 'offline'} </p>
-
-                   
+                    <div className={props.on ? 'status-online' : 'status-offline'}></div>
+                    <p className='status-text'>{props.online ? 'online' : 'offline'}</p>             
                 </div>
-            </h4>            
-        
-
-
-
+            </h4>
         </div>
+
     );
 }
-
-
-
-
-
-
+Contact.propTypes = {
+    characterFirstName: PropTypes.string,
+    characterLastName:  PropTypes.string,
+    on: PropTypes.bool,
+    online: PropTypes.bool,
+}    
 export default Contact;
